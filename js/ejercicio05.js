0937425a657601c43722cc20dc33ef040bea33c2
Izanarraez <104890144+Izanarraez@users.noreply.github.com>
2023-01-24 21:39:37 +0100

    r

--- js/ejercicio05.js
+++ js/ejercicio05.js
@@ -1,10 +1,9 @@
-document.getElementsByTagName("h3")[0].addEventListener("click", (e)=>{
+/*document.getElementsByTagName("h3")[0].addEventListener("click", (e)=>{
     let a = document.getElementsByTagName("h2")[0].innerHTML;
-    let b =  document.getElementsByTagName("ul")[0].value.innerHTML;
+    let b =  document.getElementsByTagName("ul")[0].value;
     let c = `Has elegido ${e.target.id} que se imparte en turno de ${a}
     Los nº de grupos que se imparten son 3: ${b}`;
-    //document.createElement("footer").appendChild(c);
-    alert(c)
+    let d = document.createElement("div").appendChild("p").appendChild(c);
 });
 document.getElementsByTagName("h3")[1].addEventListener("click", (e)=>{
     let a = document.getElementsByTagName("h2")[0];
@@ -26,4 +25,16 @@ document.getElementsByTagName("h3")[3].addEventListener("click", (e)=>{
     let c = `Has elegido ${e.target.id} que se imparte en turno de ${a}
     Los nº de grupos que se imparten son 3: ${b}`;
     document.createElement("div").appendChild(c); 
-});
+});*/
+
+const h3 = document.getElementsByTagName("h3");
+const h2 = document.getElementsByTagName("h2");
+const ul =  document.getElementsByTagName("ul");
+
+for(let i=0;i<h3.length;i++){
+    h3[i].addEventListener("click",(e)=>{
+            alert(e.target.value);
+    });
+    //let crear = document.createElement("div");
+    //crear.appendChild();
+}
